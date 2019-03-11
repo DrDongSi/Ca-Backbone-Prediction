c87634e22ec546dba0e90eb453c71dfead3ea631
Jonas Pfab <jpfab@uw.edu>
2019-03-11 14:56:28 -0700

    Update threshold finding strategy

--- preprocessing/find_threshold.py
+++ preprocessing/find_threshold.py
@@ -1,7 +1,27 @@
+"""Responsible for automatically finding threshold level if it was not provided
+by the user
+
+The threshold value is later used to set all values below the threshold to zero
+in order to produce a density map which resembles those which were generated
+by the mrc2pdb script with which the CNN was trained.
+
+Threshold values are calculated using two different methods. First, we want to
+find a threshold value such that the density map has a surface area to volume
+ratio of 0.8. Secondly, we want to find a threshold value such that the ratio of
+number of values larger than the threshold to number of non-zero values is
+0.484. The median of both numbers is then used as the threshold value and stored
+in a file at /preprocessing/threshold.
+"""
+
 import subprocess
 from scipy.optimize import root_scalar
 import os
 import json
+import mrcfile
+from copy import deepcopy
+import numpy as np
+
+__author__ = 'Jonas Pfab'
 
 
 def update_paths(paths):
@@ -9,18 +29,46 @@ def update_paths(paths):
 
 
 def execute(paths):
-    if 'thresholds_file' in paths:
-        emdb_id = paths['input'].split('/')[-2]
-        with open(paths['thresholds_file']) as f:
-            thresholds = json.load(f)
+    """Finds threshold level and writes it to threshold file if no threshold
+    level was provided by the user"""
+    if is_threshold_provided(paths):
+        return
 
-        if emdb_id in thresholds:
-            return
+    map_data = deepcopy(mrcfile.open(paths['cleaned_map'], mode='r').data).ravel()
+    num_non_zero_values = count_values(map_data, 0)
 
-    threshold = root_scalar(lambda t: 0.7 - sav(t, paths), bracket=[0, 10]).root
+    # Find threshold value such that the surface area to volume ratio is 0.7
+    threshold1 = root_scalar(lambda t: 0.8 - sav(t, paths),
+                             bracket=[0, 10]).root
+    # Finding threshold value such that the ratio of number of values larger
+    # than the threshold to number of non-zero values is 0.4
+    threshold2 = root_scalar(lambda t: 0.484 - (count_values(map_data, t) / num_non_zero_values),
+                             bracket=[0, 10]).root
+
+    threshold = (threshold1 + threshold2) / 2
 
     with open(paths['threshold'], 'w') as f:
         f.write(str(threshold))
+
+    raise Exception('Done')
+
+
+def count_values(map_data, threshold):
+    """Count number of values in 'map_data' larger than given 'threshold'
+
+    Parameters
+    ----------
+    map_data: array
+        Protein density map data
+    threshold: float
+        Threshold level which values have to exceed in order to be counted
+
+    Returns
+    ----------
+    num_values: float
+        The number of values which are larger than the given 'threshold'
+    """
+    return len(np.where(map_data > threshold)[0])
 
 
 def sav(threshold, paths):
@@ -48,14 +96,15 @@ def sav(threshold, paths):
     chimera_script.close()
 
     output = subprocess.check_output(['/usr/local/bin/chimera', '--nogui', chimera_script.name])
-    volume, surface_area = parse_chimera_output(output)
+    volume, surface_area = parse_sav(output)
 
     os.remove(chimera_script.name)
 
     return float('inf') if volume == 0 else surface_area / volume
 
 
-def parse_chimera_output(output):
+def parse_sav(output):
+    """Parses surface area to volume ratio from given chimera output"""
     volume, area = None, None
     lines = str(output).split('\\n')
     for line in lines:
@@ -65,3 +114,16 @@ def parse_chimera_output(output):
             volume = float(line.split(' = ')[-1])
 
     return volume, area
+
+
+def is_threshold_provided(paths):
+    """Checks if threshold value is already provided by user"""
+    if 'thresholds_file' in paths:
+        emdb_id = paths['input'].split('/')[-2]
+        with open(paths['thresholds_file']) as f:
+            thresholds = json.load(f)
+
+        if emdb_id in thresholds:
+            return True
+
+    return False
