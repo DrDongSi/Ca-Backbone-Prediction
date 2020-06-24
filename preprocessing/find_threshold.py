039c4d04200a27d7b2d1d3ca0d8ef89575a3d8fe
ahnaka <50560195+Silver7873@users.noreply.github.com>
2020-06-24 14:53:28 -0700

    find_threshold changes
    Changes the pathing for the chimera symbolic link

--- preprocessing/find_threshold.py
+++ preprocessing/find_threshold.py
@@ -33,7 +33,6 @@ def execute(paths):
     level was provided by the user"""
     if is_threshold_provided(paths):
         return
-
     map_data = deepcopy(mrcfile.open(paths['cleaned_map'], mode='r').data).ravel()
     num_non_zero_values = count_values(map_data, 0)
 
@@ -93,7 +92,7 @@ def sav(threshold, paths):
                          'measure area #0\n')
     chimera_script.close()
 
-    output = subprocess.check_output(['/usr/local/bin/chimera', '--nogui', chimera_script.name])
+    output = subprocess.check_output([paths['chimera_path'], '--nogui', chimera_script.name])
     volume, surface_area = parse_sav(output)
 
     os.remove(chimera_script.name)
@@ -119,6 +118,7 @@ def is_threshold_provided(paths):
     if 'thresholds_file' in paths:
         emdb_id = paths['input'].split('/')[-2]
         with open(paths['thresholds_file']) as f:
+            print(f)
             thresholds = json.load(f)
 
         if emdb_id in thresholds:
